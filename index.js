400acaabbecdd7ef1c37037967612ceebe0218b4
mahfuzHasan2003 <mahfuzhasan2003@yahoo.com>
2024-12-27 00:37:29 +0600

    added jwt functionality

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 const express = require("express");
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 3000;
 require("dotenv").config();
 
-app.use(cors());
+app.use(
+   cors({
+      origin: "http://localhost:5173",
+      credentials: true,
+      optionsSuccessStatus: 200,
+   })
+);
 app.use(express.json());
+app.use(cookieParser());
 
 const uri = `mongodb+srv://${process.env.DB_user}:${process.env.DB_password}@cluster0.xggde.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -17,6 +26,19 @@ const client = new MongoClient(uri, {
       deprecationErrors: true,
    },
 });
+
+// verify token
+const verifyToken = (req, res, next) => {
+   const token = req.cookies?.token;
+   if (!token) return res.status(401).send({ message: "unauthorized access" });
+   jwt.verify(token, process.env.SECRET_KEY, (error, decoded) => {
+      if (error) {
+         return res.status(401).send({ message: "unauthorized access" });
+      }
+      req.user = decoded;
+      next();
+   });
+};
 
 async function run() {
    try {
@@ -40,19 +62,27 @@ async function run() {
       app.get("/all-vol-need-posts", async (req, res) => {
          const sort = {};
          const showAtMost = parseInt(req.query.limit);
-         const searchText = req.query.search;
-         const email = req.query.email;
-         let query = {};
-         if (searchText)
-            query = { post_title: { $regex: searchText, $options: "i" } };
-         if (email) query = { organizer_email: email };
+         const searchText = req.query.search || "";
+         let searchQuery = {
+            post_title: { $regex: searchText, $options: "i" },
+         };
          const sortBy = req.query.sortby;
          if (sortBy === "dateAscending") sort.deadline = 1;
          const result = await allVolNeedPostCollection
-            .find(query)
+            .find(searchQuery)
             .limit(showAtMost)
             .sort(sort)
             .toArray();
+         res.send(result);
+      });
+
+      // get user all vol need posts
+      app.get("/user-vol-need-posts/", verifyToken, async (req, res) => {
+         const email = req.query.email;
+         const result = await allVolNeedPostCollection
+            .find({ organizer_email: email })
+            .toArray();
+         console.log(req.user?.email);
          res.send(result);
       });
 
@@ -93,7 +123,7 @@ async function run() {
       });
 
       // get --> all to be vol request by user
-      app.get("/to-be-vol-req", async (req, res) => {
+      app.get("/to-be-vol-req", verifyToken, async (req, res) => {
          const email = req.query.email;
          const result = await toBeVolReqCollection
             .find({ "req_user.email": email })
@@ -125,6 +155,28 @@ async function run() {
             }
          );
          res.send(result);
+      });
+
+      // JWT token varification
+      app.post("/jwt", async (req, res) => {
+         const email = req.body;
+         const token = jwt.sign(email, process.env.SECRET_KEY, {
+            expiresIn: "30d",
+         });
+         res.cookie("token", token, {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === "production",
+            sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+         }).send({ success: true });
+      });
+
+      // remove cookies after logout
+      app.get("/clear-token", async (req, res) => {
+         res.clearCookie("token", {
+            maxAge: 0,
+            secure: process.env.NODE_ENV === "production",
+            sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+         }).send({ success: true });
       });
    } catch (err) {
       console.error(err);
